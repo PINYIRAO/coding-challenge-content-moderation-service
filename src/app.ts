555c0d7da0096d5591caa57f0c91e1b93748934f
pinyirao <prao2@rrc.ca>
2025-03-18 13:19:19 -0500

    feature: apply the helmet for application security

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,34 @@
 import express, { Express, Request, Response } from "express";
 import moderationRoutes from "./api/v1/routes/moderationRoutes";
+import helmetMiddleware from "./api/v1/middleware/helmet";
+import helmet from "helmet";
 
 const app: Express = express();
+
+// app.use(helmet());
 app.use(express.json());
 
 /**
  * Mount moderation routes on /api/v1/moderation
  */
-app.use("/api/v1/moderation", moderationRoutes);
+// app.use("/api/v1/moderation", moderationRoutes);
+app.use("/api/v1/moderation", helmetMiddleware, moderationRoutes);
+// app.use(
+//   "/api/v1/moderation",
+//   helmet({
+//     contentSecurityPolicy: {
+//       directives: {
+//         "default-src": ["'self'"],
+//       },
+//     },
+//     xPoweredBy: false,
+//     xXssProtection: true,
+//     referrerPolicy: {
+//       policy: ["origin"],
+//     },
+//   }),
+//   moderationRoutes
+// );
 
 /**
  * Default error handler for unmatched routes
